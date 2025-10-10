cbad2adccfbec08b28732f4b27ab0c69a90a744b
Esteban Herrera <stv.herrera@gmail.com>
2025-10-10 17:39:02 -0500

    Add new file pg_backup_daily_remote.sh

---
+++ pg_backup_daily_remote/pg_backup_daily_remote.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+# Script Name: PostgreSQL Daily Backup + Remote Upload
+# Author:      Esteban Herrera Castro
+# Email:       stv.herrera@gmail.com
+# Date:        10/10/2025
+# ------------------------------------------------------------------------------
+# Description:
+# It automatically backs up all PostgreSQL databases daily, with compression,
+# timestamps, and automatic cleanup of old backups.
+# ------------------------------------------------------------------------------
+
