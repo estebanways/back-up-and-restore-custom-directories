44846c47c3bcd44d798264c294a3920d383bdc2f
Esteban Herrera <stv.herrera@gmail.com>
2025-10-10 16:01:07 -0500

    Remove restore code from back up script.

--- back_up_and_restore_custom_directories_using_single_tarball/back_up_custom_directories_using_single_tarball.sh
+++ back_up_and_restore_custom_directories_using_single_tarball/back_up_custom_directories_using_single_tarball.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 
-# ------------------------------------------------------------
-# Script Name: Backup and Restore Custom Directories
+# ------------------------------------------------------------------------------
+# Script Name: Backup Custom Directories
 # Author:      Esteban Herrera Castro
 # Email:       stv.herrera@gmail.com
 # Date:        05/18/2024
-# ------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Description:
-# This script compresses specified directories into a single
-# .tgz archive file and logs any errors encountered during the
-# process. It also includes optional commands for copying the
-# archive to a specified backup directory and for restoring
-# the directories from the archive.
+# This script compresses specified directories into a single .tgz archive file
+# and logs any errors encountered during the process. It also includes optional
+# commands for copying the archive to a specified backup directory.
 #
 # BACK UP
 # 1. Change to the directory where you want the TGZ and log files.
 # 2. Add new directories or remove them as required from the list.
 # 3. Compress the directories to a new archive dirs.tgz.
 # 4. Copy the archive to your storage drive(s).
-#
-# RESTORE
-# 1. Go to the root of the file system.
-# 2. Extract the archive.
 # ------------------------------------------------------------
 
-# BACK UP
 # Change to the directory where you want the TGZ and log files to be stored
 cd || exit
 
@@ -33,10 +26,3 @@ sudo tar -cvzpf dirs.tgz "$HOME"/.config/BraveSoftware/ "$HOME"/config/ "$HOME"/
 
 # Copy the archive to your storage drive(s) (uncomment and set the correct path)
 #cp -dpR dirs.tgz /path/to/a/backup/directory
-
-# RESTORE
-# Go to the root of the file system (uncomment and run as needed)
-#cd /
-
-# Extract the archive (uncomment and set the correct path)
-#tar -xvzpf /path/to/dirs.tgz
