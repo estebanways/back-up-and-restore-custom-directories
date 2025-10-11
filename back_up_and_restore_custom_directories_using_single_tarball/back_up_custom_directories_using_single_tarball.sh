a1e5c6f7a4340f654612113ce110adaea87c6a25
Esteban Herrera <stv.herrera@gmail.com>
2025-10-10 21:37:37 -0500

    Move further steps to readme.

--- back_up_and_restore_custom_directories_using_single_tarball/back_up_custom_directories_using_single_tarball.sh
+++ back_up_and_restore_custom_directories_using_single_tarball/back_up_custom_directories_using_single_tarball.sh
@@ -15,14 +15,10 @@
 # 1. Change to the directory where you want the TGZ and log files.
 # 2. Add new directories or remove them as required from the list.
 # 3. Compress the directories to a new archive dirs.tgz.
-# 4. Copy the archive to your storage drive(s).
-# ------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # Change to the directory where you want the TGZ and log files to be stored
 cd || exit
 
 # Compress the specified directories into dirs.tgz and log errors to dirs.log
 sudo tar -cvzpf dirs.tgz "$HOME"/.config/BraveSoftware/ "$HOME"/config/ "$HOME"/.local/share/nvim/site/ "$HOME"/Dev/ "$HOME"/DevDocs/ "$HOME"/Documents/ "$HOME"/Downloads/ /etc/ 2>dirs.log
-
-# Copy the archive to your storage drive(s) (uncomment and set the correct path)
-#cp -dpR dirs.tgz /path/to/a/backup/directory
